51440b78afb4e6dae992e984c71e41886e5e26ef
Lima-Heco <heinrichprog.66@gmail.com>
2024-03-22 13:17:09 +0100

    amelioration de la vue BFC_1

--- src/BFC_1/Bot_read/pente_view.rs
+++ src/BFC_1/Bot_read/pente_view.rs
@@ -43,8 +43,35 @@ pub mod tendance_view {
             return -1;
         }
         //estime la pente potentielle apartire de la derniere. si elle est egale a l'ancienne reinitialisation et actualisation de l'ancienne
-        pub fn get_potential() {
-            
+        pub fn get_potential(self: &mut Self, btc_price: Arc<RwLock<get_bitcoin::btcprice>>) {
+            let mut time;
+            if self.temp.initialized == false {
+                self.temp.start_price = self.tableau[0].end_price;
+                self.temp.start_timestamp = self.tableau[0].end_timestamp;
+                self.temp.initialized = true;
+            }
+            if true {
+                let btc_price_clone = Arc::clone(&btc_price);
+                let price_guard = btc_price_clone.read().unwrap();
+                self.temp.end_timestamp = price_guard.timestamp;
+                self.temp.end_price = price_guard.btctousd;
+                time = price_guard.next_time;
+            }
+            if self.temp.size < 2 {
+                self.temp.size = 2;
+            }
+            self.temp.valeure_de_pente = pente::calculate_slope(self.temp.start_timestamp, self.temp.start_price, self.temp.end_timestamp, self.temp.end_price);
+            if self.next_time != time
+            {
+                let pourcentage_dacceptaition = (self.tableau[0].valeure_de_pente * 10.0 \ 100.0);
+                if (seld.temp.valeure_de_pente >= (self.tableau[0].valeure_de_pente - pourcentage_dacceptaition) && seld.temp.valeure_de_pente <= (self.tableau[0].valeure_de_pente + pourcentage_dacceptaition)) {
+                    //met a joure le dernier element du tableau et reinitialise temp.
+                } else if (self.temp.size > 3) {
+                    //si la taille de temps est supperieur a 3, decale les elements du tableau et l'ajoute au debut.
+                } else {
+                    //temp size += 1
+                }
+            }
         }
     }
 }
